4a85ce15d2a6abc41d4a573aaab93b0d9d0919b8
Bennett Smrdel <smrdelb@gmail.com>
2022-04-22 12:05:43 -0500

    created the nav component and added it to the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
+import Nav from '../Nav/Nav';
 import './Header.css';
 
 function Header() {
-  return (
-    <center>
-      <h1>Bennett Smrdel</h1>
-      <h3>Full Stack Developer</h3>
-    </center>
-  );
+    return (
+      <div>
+          <Nav />
+          {/* Header content */}
+          <center>
+            <h1>Bennett Smrdel</h1>
+            <h3>Full Stack Developer</h3>
+          </center>
+      </div>
+    );
 }
 
 export default Header;
