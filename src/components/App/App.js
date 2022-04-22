03ffbfb0f4208b0931c29f9cfc22ab837fdfd8c5
Bennett Smrdel <smrdelb@gmail.com>
2022-04-22 12:01:31 -0500

    added header component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
+import Header from '../Header/Header';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h1>BEAN</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <center>
+      <h1>Bennett Smrdel</h1>
+      <h3>Full Stack Developer</h3>
+    </center>
+  );
+}
+
+export default Header;
