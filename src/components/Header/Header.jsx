685188f75bbbcd2baa31ec69a3b39abef816e3b4
Bennett Smrdel <smrdelb@gmail.com>
2022-04-24 09:50:11 -0500

    added a navbar, and made it responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,14 +3,16 @@ import './Header.css';
 
 function Header() {
     return (
-      <div>
-          <Nav />
-          {/* Header content */}
-          <center>
-            <h1>Bennett Smrdel</h1>
-            <h3>Full Stack Developer</h3>
-          </center>
-      </div>
+        <div>
+            <Nav />
+            {/* Header content */}
+            <center>
+                <header className='header-container'>
+                    <h1>Bennett Smrdel</h1>
+                    <h3>Full Stack Developer</h3>
+                </header>
+            </center>
+        </div>
     );
 }
 
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,7 +2,7 @@ import './Nav.css';
 
 function Nav() {
     return (
-        <div>
+        <>
             <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" id="navbar">
                 <div class="container-fluid">
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,7 +23,7 @@ function Nav() {
                     </div>
                 </div>
             </nav>
-        </div>
+        </>
     );
 }
 
